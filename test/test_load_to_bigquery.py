655f39016ca4971ffa5036d4470bd78384d586ad
Sahithya Karangula <68002637+sahithyakarangula25@users.noreply.github.com>
2024-04-10 18:05:59 +0100

    Update test_load_to_bigquery.py
    This commit introduces a unit test for the load_to_database module, specifically targeting the load_data_into_table function. Using the patch decorator, the database connection is mocked to prevent actual database interactions during testing. Mock objects for the connection and cursor are created using MagicMock. The test data consists of a single post entry. The function under test, load_data_into_table, is called with the mock cursor and test data. Assertions are made to ensure that the execute method of the cursor is called once and that the commit method of the connection is also called once. This unit test validates the functionality of the data loading process, ensuring that data is correctly inserted into the database.

--- test/test_load_to_bigquery.py
+++ test/test_load_to_bigquery.py
@@ -1,1 +1,30 @@
-# Put your unit tests here
+import unittest
+from unittest.mock import patch, MagicMock
+import sys
+
+# Add the directory containing the etl_pipeline package to sys.path
+sys.path.insert(1, 'C://Users//DIVYA//PycharmProjects//SDE-Tech-Task-xMpCO//etl_pipeline')
+
+from etl_pipeline import load_to_database
+
+class TestLoadToDatabase(unittest.TestCase):
+
+    @patch('load_to_database.sqlite3.connect')
+    def test_load_data_into_table(self, mock_connect):
+        # Mock database connection
+        mock_conn = MagicMock()
+        mock_cursor = MagicMock()
+        mock_connect.return_value = (mock_conn, mock_cursor)
+
+        # Test data
+        data = [{'id': 1, 'title': 'Post 1', 'body': 'Body of post 1', 'status': 'lengthy', 'user': {'id': 1}}]
+
+        # Call the function under test
+        load_to_database.load_data_into_table(mock_cursor, data)
+
+        # Assertions
+        mock_cursor.execute.assert_called_once()
+        mock_conn.commit.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
