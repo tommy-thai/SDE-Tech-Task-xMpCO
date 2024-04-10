96d464d37a5323b925289a3ca6c753f990680878
Sahithya Karangula <68002637+sahithyakarangula25@users.noreply.github.com>
2024-04-10 18:05:05 +0100

    Update test_api_client.py
    This commit includes a unit test for the API client module, specifically targeting the get_posts function. The test mocks the HTTP GET request to the JSONPlaceholder API using the requests.get function, simulating a successful response with a single post. The test verifies that the get_posts function correctly processes the API response and returns the expected post data. Assertions are included to validate the ID, title, and body of the retrieved post. This unit test ensures the reliability and correctness of the API client functionality, contributing to the overall quality and robustness of the ETL pipeline.

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -1,1 +1,27 @@
 # Put your unit tests here
+import unittest
+from unittest.mock import patch
+import sys
+sys.path.insert(1, 'C://Users//DIVYA//PycharmProjects//SDE-Tech-Task-xMpCO//etl_pipeline')
+from etl_pipeline import api_client
+
+class TestAPIClient(unittest.TestCase):
+
+    @patch('api_client.requests.get')
+    def test_get_posts(self, mock_get):
+        # Mock API response
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = [{'id': 1, 'title': 'Post 1', 'body': 'Body of post 1'}]
+
+        # Call the function under test
+        posts = api_client.get_posts()
+
+        # Assertions
+        self.assertEqual(len(posts), 1)
+        self.assertEqual(posts[0]['id'], 1)
+        self.assertEqual(posts[0]['title'], 'Post 1')
+        self.assertEqual(posts[0]['body'], 'Body of post 1')
+
+
+if __name__ == '__main__':
+    unittest.main()
